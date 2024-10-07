c751766ce1451422cedd4b1e01eac9be660b94d7
Delonte <delonte@ElijahAmens-MacBook-Pro.local>
2024-10-07 15:33:51 -0400

    updates

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,42 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const Blog = require('../models/blog');
+const isSignedIn = require('../middleware/is-signed-in');
+const passUserToView = require('../middleware/pass-user-to-view');
 
+// Use middleware to pass the user to views
+router.use(passUserToView);
 
-
+// Public route: View all blogs
 router.get('/', async (req, res) => {
   try {
-    const blogs = await Blog.find(); 
-    res.render('blogs/index.ejs', { blogs }); // Pass blogs to the view
+    const blogs = await Blog.find();
+    res.render('blogs/index.ejs', { blogs });
   } catch (error) {
     console.error('Error fetching blogs:', error);
-    res.redirect('/'); // Redirect on error
+    res.redirect('/');
   }
 });
 
-router.get('/new', (req, res) => {
-  res.render('blogs/new.ejs');
-});
-
-router.post('/', async (req, res) => {
-  try {
-    const newBlog = new Blog({
-      ...req.body,
-      author: req.session.user._id,
-      date: req.body.date || Date.now(),
-    });
-    await newBlog.save();
-    res.redirect('/blogs');
-  } catch (error) {
-    console.error('Error creating blog:', error);
-    res.redirect('/blogs/new');
-  }
-});
-
+// Public route: View a single blog post
 router.get('/:blogId', async (req, res) => {
   try {
     const blog = await Blog.findById(req.params.blogId).populate('author');
-    if (!blog) return res.status(404).send('Blog not found');
+    if (!blog) {
+      return res.status(404).send('Blog not found');
+    }
     res.render('blogs/show.ejs', { blog });
   } catch (error) {
     console.error('Error fetching blog:', error);
@@ -44,11 +32,43 @@ router.get('/:blogId', async (req, res) => {
   }
 });
 
-// Edit blog
+// Routes requiring authentication
+router.use(isSignedIn);
+
+// Protected route: Show form to create a new blog
+router.get('/new', (req, res) => {
+  res.render('blogs/new.ejs');
+});
+
+// Protected route: Create a new blog
+router.post('/', async (req, res) => {
+  try {
+    const { title, content, date } = req.body;
+    const authorId = req.session.user._id; // Get the author ID from the session
+
+    const newBlog = new Blog({
+      title,
+      content,
+      date: date || Date.now(),
+      author: authorId // Assign the correct author ID
+    });
+
+    await newBlog.save();
+    res.redirect('/blogs'); // Redirect to the blogs index after saving
+  } catch (error) {
+    console.error('Error creating blog:', error);
+    res.status(400).send('Error creating blog');
+  }
+});
+
+
+// Protected route: Show form to edit a blog
 router.get('/:blogId/edit', async (req, res) => {
   try {
     const blog = await Blog.findById(req.params.blogId);
-    if (!blog) return res.status(404).send('Blog not found');
+    if (!blog) {
+      return res.status(404).send('Blog not found');
+    }
     res.render('blogs/edit.ejs', { blog });
   } catch (error) {
     console.error('Error fetching blog for edit:', error);
@@ -56,21 +76,27 @@ router.get('/:blogId/edit', async (req, res) => {
   }
 });
 
-// Update: 
+// Protected route: Update a blog
 router.put('/:blogId', async (req, res) => {
   try {
-    await Blog.findByIdAndUpdate(req.params.blogId, req.body, { new: true });
-    res.redirect('/blogs');
+    const updatedBlog = await Blog.findByIdAndUpdate(req.params.blogId, req.body, { new: true });
+    if (!updatedBlog) {
+      return res.status(404).send('Blog not found');
+    }
+    res.redirect(`/blogs/${req.params.blogId}`);
   } catch (error) {
     console.error('Error updating blog:', error);
     res.redirect(`/blogs/${req.params.blogId}/edit`);
   }
 });
 
-// Delete: 
+// Protected route: Delete a blog
 router.delete('/:blogId', async (req, res) => {
   try {
-    await Blog.findByIdAndDelete(req.params.blogId);
+    const deletedBlog = await Blog.findByIdAndDelete(req.params.blogId);
+    if (!deletedBlog) {
+      return res.status(404).send('Blog not found');
+    }
     res.redirect('/blogs');
   } catch (error) {
     console.error('Error deleting blog:', error);
